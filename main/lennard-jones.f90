cb2ed772424396d7c2c3b149910fd10b6c410f90
Cerberto <ap.cerberus@gmail.com>
2014-01-11 13:37:30 +0100

    -

--- main/lennard-jones.f90
+++ main/lennard-jones.f90
@@ -1,14 +1,3 @@
-program LJ
-    
-    
-    use jackknife, only: JK, JK_init, JK_cluster, JK_function
-    implicit none
-    
-    type(particle), dimension(:), pointer :: ptcls
-    
-
-end program LJ
-
 module LJ_global
     implicit none
     
@@ -17,23 +6,43 @@ module LJ_global
     end type particle
     
     ! cube size
-    real, parameter :: L = 100.0
+    real :: L
     integer :: N
-    
-    interface
-        subroutine init (p, s)
-            real :: s
-            type(particle), dimension(:), pointer :: p
-        end subroutine init
-    end interface
 
 contains
 
     subroutine particle_init (ptcls, side)
         real :: side    ! cube side
-        type(particle), dimension(:), pointer :: ptcls  ! set of particles
+        type(particle), dimension(:) :: ptcls  ! set of particles
         real :: lspc    ! lattice spacing
+        integer :: i,j,k
+        real :: temp
+        integer :: npsd     ! particles per side
+        real :: x,y,z
+        
+        ! ATTENTION : may particles go outside the box?
+        temp = size(ptcls)**(1/3.0)
+        lspc = side/(temp+1)
+        npsd = int(temp)
+        
         
     end subroutine particle_init
     
 end module LJ_global
+
+
+program LJ
+
+    ! use jackknife, only: JK, JK_init, JK_cluster, JK_function
+    use LJ_global, only: particle, particle_init, L, N
+    implicit none
+    
+    type(particle), dimension(:), allocatable :: ptcls
+    
+    print *, "Box side: "
+    read *, L
+    print *, "Number of particles: "
+    read *, N
+    
+
+end program LJ
