2956474ee6a12465fa8e4465ba17b0cd10f28e62
Cerberto <ap.cerberus@gmail.com>
2014-01-11 17:38:33 +0100

    initialization of particles

--- main/lennard-jones.f90
+++ main/lennard-jones.f90
@@ -2,7 +2,7 @@ module LJ_global
     implicit none
     
     type :: particle
-        real, dimension(3) :: x
+        real :: x, y, z
     end type particle
     
     ! cube size
@@ -20,12 +20,28 @@ contains
         integer :: npsd     ! particles per side
         real :: x,y,z
         
-        ! ATTENTION : may particles go outside the box?
+        ! ATTENTION : may particles go outside the box? check with numbers
         temp = size(ptcls)**(1/3.0)
-        lspc = side/(temp+1)
-        npsd = int(temp)
+        lspc = side/(temp+2)
+        npsd = int(temp)+1
         
-        
+        ! initialization of particles inside a cubic box centered in 0:
+        ! particles are distributed on a cubic lattice with spacing lspc
+        x = -side + lspc*npsd/2
+        y = x
+        z = x
+        do i=1, npsd, 1
+            x = x + lspc
+            do j=1, npsd, 1
+                y = y + lspc
+                do k=1, npsd, 1
+                    z = z + lspc
+                    ptcls(i+j+k-2)%x = x
+                    ptcls(i+j+k-2)%y = y
+                    ptcls(i+j+k-2)%z = z
+                end do
+            end do      
+        end do
     end subroutine particle_init
     
 end module LJ_global
@@ -44,5 +60,7 @@ program LJ
     print *, "Number of particles: "
     read *, N
     
+    allocate(ptcls(N))
+    call particle_init(ptcls,L)
 
 end program LJ
