b98d53bebcb596e04f041026cc5f5dbd33095263
Cerberto <ap.cerberus@gmail.com>
2014-02-13 22:25:25 +0100

    camon!

--- main/linpot_lattice.c
+++ main/linpot_lattice.c
@@ -26,6 +26,8 @@ int NTH;		/* number of sweeps needed for thermalization */
 int NSW;		/* number of "effective" sweeps */
 int NBIN;		/* number of sweeps skipped in order to avoid correlation */
 int TMAX;		/* maximum time delay for autocorrelation */
+int NCONV;		/* number of iterations needed to convergence */
+int NAV;		/* number of iterations to average on after convergence */
 
 double delta;	/* hypercube side */
 double vpar;	/* variational parameter of the trial wave funct */
@@ -38,37 +40,41 @@ double trialWF (double x);
 double localenergy (double x);
 
 
-int main (int argc, char *argv[]) {	
+int main (int argc, char *argv[]) {
 
 	int NDAT;	/* size of the data sample */
-	int sw, i;
-	double en_sum, enld_sum, ld_sum, site, vpar_init, vpar_old, Dvpar, accuracy;
+	int sw, i, counter;
+	double en_sum, enld_sum, ld_sum, en_wsum, en_varsum, \
+		site, vpar_init, Dvpar; // accuracy;
 	double *site_p;
 		site_p = &site;
 
 	/* jackknife structure for the energy estimator */
-	cluster energy, enldcorr, ld;
+	cluster energy, enldcorr, ld, vpar_av;
 
 	/* array needed to compute the autocorrelation */
 	double *autocorr;
 	
 	/* Name of the output files */
-	char *therm_name, *integral_name, *autocorr_name, *distrib_name;
+	char *therm_name, *integral_name, *autocorr_name, *distrib_name, *vpar_name;
 		therm_name		= malloc(100*sizeof(char));
 		integral_name	= malloc(100*sizeof(char));
 		autocorr_name	= malloc(100*sizeof(char));
 		distrib_name	= malloc(100*sizeof(char));
+		vpar_name		= malloc(100*sizeof(char));
 	sprintf(therm_name, "lp_output/thermalization.dat");
 	sprintf(integral_name, "lp_output/expectationvalues.dat");
 	sprintf(autocorr_name, "lp_output/autocorrelation.dat");
 	sprintf(distrib_name, "lp_output/distribution.dat");
+	sprintf(vpar_name, "lp_output/variationalparameter.dat");
 	
 	/* Output streams */
-	FILE *therm_file, *integral_file, *autocorr_file, *distrib_file;
+	FILE *therm_file, *integral_file, *autocorr_file, *distrib_file, *vpar_file;
 		therm_file		= fopen(therm_name, "w");
 		integral_file	= fopen(integral_name, "w");
 		autocorr_file	= fopen(autocorr_name, "w");
 		distrib_file	= fopen(distrib_name, "w");
+		vpar_file		= fopen(vpar_name, "w");
 			
 	/* Initialization of the randomizer */
 	srand(time(NULL));
@@ -78,6 +84,8 @@ int main (int argc, char *argv[]) {
 	scanf("%d",&NSW);
 	scanf("%d",&NBIN);
 	scanf("%d",&TMAX);
+	scanf("%d",&NCONV);
+	scanf("%d",&NAV);
 		
 	scanf("%lf",&delta);
 	scanf("%lf",&L);
@@ -85,8 +93,8 @@ int main (int argc, char *argv[]) {
 	scanf("%lf",&T);
 	scanf("%lf",&V);
 	scanf("%lf",&vpar_init);
-	scanf("%lf",&accuracy);
 	scanf("%lf",&Dvpar);
+	// scanf("%lf",&accuracy);
 
 	NDAT = NSW/NBIN;
 	printf("Number of lattice sites : %d \n", (int)L);
@@ -94,10 +102,12 @@ int main (int argc, char *argv[]) {
 	JKinit(&energy, NDAT);
 	JKinit(&enldcorr, NDAT);
 	JKinit(&ld, NDAT);
+	JKinit(&vpar_av, NAV);
 	autocorr = malloc(NSW*sizeof(double));
 
 	vpar = vpar_init;
-	do {
+	en_wsum = en_varsum = 0;
+	for(counter=-NCONV; counter<NAV; counter++) {
 
 		/* Process is left free for a certain number NTH of sweeps:
 	 	* no data are used for estimating the integral
@@ -116,11 +126,12 @@ int main (int argc, char *argv[]) {
 			site = *site_p;
 
 			/* Print site to see the distribution */
-			if (vpar == vpar_init)
+			if (vpar == vpar_init) {
 				fprintf(distrib_file, "%3.1lf\n", site);
+				/* store the value of 'site' to compute the autocorrelation */
+				autocorr[sw] = site;
+			}
 
-			/* store the value of 'site' to compute the autocorrelation */
-			autocorr[sw] = site;
 			en_sum += localenergy(site)/NBIN;
 			ld_sum += -site/NBIN;
 			enld_sum += -site*localenergy(site)/NBIN;
@@ -135,27 +146,39 @@ int main (int argc, char *argv[]) {
 			}
 		}
 				
-		/* Expectation value and variance computed
-	 	* with the jackknife re-sampling method
-	 	**/
+		/* Average and variance calculated with the jackknife re-sampling */
 		JKcluster(&energy);
 		JKcluster(&enldcorr);
 		JKcluster(&ld);
-		fprintf(integral_file, "%lf\t%.10e\t%.10e\t%.10e\t%.10e\t%.10e\t%.10e\n", \
-			vpar, energy.Mean, sqrt(energy.Var), \
-			enldcorr.Mean, sqrt(enldcorr.Var), \
-			ld.Mean, sqrt(ld.Var) );
 
-		vpar_old = vpar;
+		if (counter < 0)
+			fprintf(integral_file, "%lf\t%.10e\t%.10e\t%.10e\t%.10e\t%.10e\t%.10e\n", \
+				vpar, energy.Mean, sqrt(energy.Var), \
+				enldcorr.Mean, sqrt(enldcorr.Var), \
+				ld.Mean, sqrt(ld.Var) );
 		
 		/* New variational parameter calculated via Steepest Descent */
 		vpar = vpar - Dvpar*2.0*(enldcorr.Mean - energy.Mean * ld.Mean - 2*T);
-		
-	} while (fabs(vpar - vpar_old) > accuracy);
+		fprintf(vpar_file, "%d\t%.10e\n", counter, vpar);
+		if (counter >= 0) {
+			vpar_av.Vec[counter] = vpar;
+			en_wsum		+= energy.Mean/energy.Var;
+			en_varsum 	+= 1.0/energy.Var;
+		}
+	}
 
+	/* Calculate the mean variational parameter and its variance */
+	JKcluster(&vpar_av);
+	printf("Optimal variational parameter      : %lf +- %lf \n", \
+		vpar_av.Mean, sqrt(vpar_av.Var));
+
+	/* Calculate the minimum variational energy as a weighted average */
+	printf("Average minimum variational energy : %lf +- %.10e \n\n", \
+		en_wsum/en_varsum, 1.0/en_varsum);
+
+	/* Print the autocorrelation of the process 'site' for the initial vpar */
 	for(i=0; i<TMAX+1; i++)
 		fprintf(autocorr_file, "%d\t%e\n", i, autocorrelation(autocorr, i, NDAT));
-	printf("Optimal variational parameter : %lf +- %lf \n\n", vpar, accuracy);
 
 	fclose(therm_file);
 	fclose(autocorr_file);
